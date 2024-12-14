8ce17bce5766c063565c1b05aa7d4e44bf942a14
dini777 <karlojakopov@gmail.com>
2024-12-14 14:04:17 +0100

    Testing new validation schema

--- backend/users/UpdateUserInfo/validation_schema.py
+++ backend/users/UpdateUserInfo/validation_schema.py
@@ -2,23 +2,23 @@ schema = {
     "type": "object",
     "properties": {
         "first_name": {
-            "type": "string",
+            "type": ["string", "null"],
             "minLength": 1,
             "maxLength": 50
         },
         "last_name": {
-            "type": "string",
+            "type": ["string", "null"],
             "minLength": 1,
             "maxLength": 50
         },
         "phone_number": {
-            "type": "string",
+            "type": ["string", "null"],
             "minLength": 8,
             "maxLength": 15,
             "pattern": r"^\+?[1-9]\d{1,14}$"
         },
         "tags": {
-            "type": "array",
+            "type": ["array", "null"],
             "items": {
                 "type": "string",
                 "maxLength": 255
