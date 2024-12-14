c35248b5201475f94339c1225eecb7faf686a431
dini777 <karlojakopov@gmail.com>
2024-12-14 14:20:13 +0100

    Reverting null checkings

--- backend/users/UpdateUserInfo/validation_schema.py
+++ backend/users/UpdateUserInfo/validation_schema.py
@@ -2,23 +2,23 @@ schema = {
     "type": "object",
     "properties": {
         "first_name": {
-            "type": ["string", "null"],
+            "type": "string",
             "minLength": 1,
             "maxLength": 50
         },
         "last_name": {
-            "type": ["string", "null"],
+            "type": "string",
             "minLength": 1,
             "maxLength": 50
         },
         "phone_number": {
-            "type": ["string", "null"],
+            "type": "string",
             "minLength": 8,
             "maxLength": 15,
             "pattern": r"^\+?[1-9]\d{1,14}$"
         },
         "tags": {
-            "type": ["array", "null"],
+            "type": "array",
             "items": {
                 "type": "string",
                 "maxLength": 255
