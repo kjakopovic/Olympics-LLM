586312d2821eab2c0aec1dbf896670141f162a01
dini777 <karlojakopov@gmail.com>
2024-12-22 08:16:29 +0100

    Added change password tests and did some styling changes in other tests

--- backend/users/tests/test_change_password.py
+++ backend/users/tests/test_change_password.py
@@ -9,13 +9,13 @@ import os
 if 'validation_schema' in sys.modules:
     del sys.modules['validation_schema']
 
-new_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'RegisterUser'))
+new_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'ChangePassword'))
 sys.path.append(new_path)
 
-from RegisterUser.lambda_handler import lambda_handler
+from ChangePassword.lambda_handler import lambda_handler
 
 @mock_aws
-class TestRegisterUserLambda(BaseUsersTest):
+class TestChangePasswordLambda(BaseUsersTest):
     def setUp(self):
         super().setUp()
 
@@ -28,17 +28,15 @@ class TestRegisterUserLambda(BaseUsersTest):
             {
                 "request_body": {
                     "email": "test2@mail.com",
-                    "password": "password123",
-                    "first_name": "John"
+                    "password": "password123"
                 },
-                "expected_validation_message": "data must contain ['last_name'] properties"
+                "expected_validation_message": "data must contain ['new_password'] properties"
             },
             {
                 "request_body": {
                     "email": "test2@mail.com",
                     "password": "password123",
-                    "first_name": "John",
-                    "last_name": "Doe",
+                    "new_password": "password123!",
                     "new_field": "password123"
                 },
                 "expected_validation_message": "data must not contain {'new_field'} properties"
@@ -47,8 +45,7 @@ class TestRegisterUserLambda(BaseUsersTest):
                 "request_body": {
                     "email": "test",
                     "password": "password123",
-                    "first_name": "John",
-                    "last_name": "Doe"
+                    "new_password": "password123!"
                 },
                 "expected_validation_message": "data.email must be email"
             },
@@ -56,8 +53,7 @@ class TestRegisterUserLambda(BaseUsersTest):
                 "request_body": {
                     "email": "test2@mail.com",
                     "password": "pass",
-                    "first_name": "John",
-                    "last_name": "Doe"
+                    "new_password": "password123!"
                 },
                 "expected_validation_message": "data.password must be longer than or equal to 7 characters"
             },
@@ -65,37 +61,9 @@ class TestRegisterUserLambda(BaseUsersTest):
                 "request_body": {
                     "email": "test2@mail.com",
                     "password": "password123",
-                    "first_name": "",
-                    "last_name": "Doe"
+                    "new_password": "pass"
                 },
-                "expected_validation_message": "data.first_name must be longer than or equal to 1 characters"
-            },
-            {
-                "request_body": {
-                    "email": "test2@mail.com",
-                    "password": "password123",
-                    "first_name": "John",
-                    "last_name": ""
-                },
-                "expected_validation_message": "data.last_name must be longer than or equal to 1 characters"
-            },
-            {
-                "request_body": {
-                    "email": "test2@mail.com",
-                    "password": "password123",
-                    "first_name": "JohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohn",
-                    "last_name": "Doe"
-                },
-                "expected_validation_message": "data.first_name must be shorter than or equal to 50 characters"
-            },
-            {
-                "request_body": {
-                    "email": "test2@mail.com",
-                    "password": "password123",
-                    "first_name": "John",
-                    "last_name": "DoeJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohnJohn"
-                },
-                "expected_validation_message": "data.last_name must be shorter than or equal to 50 characters"
+                "expected_validation_message": "data.new_password must be longer than or equal to 7 characters"
             }
         ]
         
@@ -113,18 +81,17 @@ class TestRegisterUserLambda(BaseUsersTest):
                 self.assertEqual(response['statusCode'], 400)
                 self.assertIn(case["expected_validation_message"], body['message'])
 
-    def test_when_user_already_exists(self):
+    def test_when_user_doesnt_exists(self):
         """
-        Test response when user already exists, shouldn't be able to register the same user.
+        Test response when user doesn't exist.
         """
         
         # Arrange
         event = {
             "body": json.dumps({
-                "email": "test@mail.com",
+                "email": "test2@mail.com",
                 "password": "password123",
-                "first_name": "John",
-                "last_name": "Doe"
+                "new_password": "password123!"
             })
         }
 
@@ -134,20 +101,41 @@ class TestRegisterUserLambda(BaseUsersTest):
         
         # Assert
         self.assertEqual(response['statusCode'], 400)
-        self.assertEqual(body['message'], "User with this email already exists. Do you want to login instead?")
+        self.assertEqual(body['message'], "User with this email does not exist. Do you want to register instead?")
 
-    def test_success(self):
+    def test_when_incorrect_password(self):
         """
-        Test response when user registers successfully.
+        Test response when user enters incorrect current password.
         """
         
         # Arrange
         event = {
             "body": json.dumps({
-                "email": "test2@mail.com",
+                "email": "test@mail.com",
+                "password": "password777",
+                "new_password": "password123!"
+            })
+        }
+
+        # Act
+        response = lambda_handler(event, {})
+        body = json.loads(response['body'])
+        
+        # Assert
+        self.assertEqual(response['statusCode'], 400)
+        self.assertEqual(body['message'], "Password is incorrect")
+
+    def test_success(self):
+        """
+        Test response when user changes password successfully.
+        """
+        
+        # Arrange
+        event = {
+            "body": json.dumps({
+                "email": "test@mail.com",
                 "password": "password123",
-                "first_name": "John",
-                "last_name": "Doe"
+                "new_password": "password123!"
             })
         }
 
@@ -157,6 +145,6 @@ class TestRegisterUserLambda(BaseUsersTest):
         
         # Assert
         self.assertEqual(response['statusCode'], 200)
-        self.assertEqual(body['message'], "User has been successfully registered")
+        self.assertEqual(body['message'], "Password has been successfully changed")
 
 sys.path.remove(new_path)
