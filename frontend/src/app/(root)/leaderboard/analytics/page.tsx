e49eff2dd5c4c9b7ea5d078287121140df796d33
dini777 <karlojakopov@gmail.com>
2025-01-31 16:46:07 +0100

    Finished graphs, ready and working

--- frontend/src/app/(root)/leaderboard/analytics/page.tsx
+++ frontend/src/app/(root)/leaderboard/analytics/page.tsx
@@ -16,23 +16,10 @@ import {
 } from "@/components/ui/chart"
 import SideBar from "@/components/SideBar"
 
-const realChartData = [
-    { sportsman: "Luke Skywalker", gold: 186, silver: 80, bronze: 50, appearance: 1000 },
-    { sportsman: "Zoran Milanovic", gold: 305, silver: 200, bronze: 100, appearance: 500 },
-    { sportsman: "Leo Messi", gold: 237, silver: 120, bronze: 500, appearance: 700 },
-    { sportsman: "Cristiano Ronaldo", gold: 73, silver: 190, bronze: 150, appearance: 1100 },
-    { sportsman: "Luka Modric", gold: 209, silver: 130, bronze: 200, appearance: 1500 },
-    { sportsman: "Domagoj Duvnjak", gold: 214, silver: 140, bronze: 250, appearance: 700 },
-]
-
-const continentData = [
-    { continent: "africa", medals: 100, fill: "var(--color-africa)" },
-    { continent: "europe", medals: 1000, fill: "var(--color-europe)" },
-    { continent: "asia", medals: 500, fill: "var(--color-asia)" },
-    { continent: "australia", medals: 300, fill: "var(--color-australia)" },
-    { continent: "north_america", medals: 250, fill: "var(--color-north_america)" },
-    { continent: "south_america", medals: 150, fill: "var(--color-south_america)" },
-]
+import {useEffect, useState} from "react"
+import LoadingSpinner from "@/components/LoadingSpinner"
+import { useRouter } from "next/navigation"
+import Cookies from "js-cookie";
 
 const continentChartConfig = {
     africa: {
@@ -45,31 +32,23 @@ const continentChartConfig = {
     },
     asia: {
       label: "Asia",
-      color: "hsl(var(--chart-1))",
+      color: "hsl(var(--chart-3))",
     },
     australia: {
       label: "Australia",
-      color: "hsl(var(--chart-2))",
+      color: "hsl(var(--chart-4))",
     },
     north_america: {
       label: "North America",
-      color: "hsl(var(--chart-1))",
+      color: "hsl(var(--chart-5))",
     },
     south_america: {
       label: "South America",
-      color: "hsl(var(--chart-2))",
+      color: "hsl(var(--chart-6))",
     }
 } satisfies ChartConfig
 
 const chartConfig = {
-  desktop: {
-    label: "Desktop",
-    color: "hsl(var(--chart-1))",
-  },
-  mobile: {
-    label: "Mobile",
-    color: "hsl(var(--chart-2))",
-  },
   gold: {
     label: "Gold medals",
     color: "hsl(var(--chart-1))",
@@ -80,7 +59,7 @@ const chartConfig = {
   },
   bronze: {
     label: "Bronze medals",
-    color: "hsl(var(--chart-1))",
+    color: "hsl(var(--chart-3))",
   },
   appearance: {
     label: "Number of appearances",
@@ -92,49 +71,212 @@ const chartConfig = {
   },
 } satisfies ChartConfig
 
-const linearChartData = [
-    { year: 1910, medals: 186 },
-    { year: 1920, medals: 305 },
-    { year: 1930, medals: 237 },
-    { year: 1940, medals: 73 },
-    { year: 1950, medals: 209 },
-    { year: 1960, medals: 214 },
-]
-
 function Analytics() {
-  return (
-    <div className="h-full bg-gradient-to-tr from-primary-200 via-primary-200 to-primary-100 flex">
-        {/* SideBar */}
-        <SideBar />
+    const router = useRouter();
+    
+    const [loading, setLoading] = useState<boolean>(true);
+    const [continentData, setContinentData] = useState<any[]>([
+        { continent: "africa", medals: 100, fill: "var(--color-africa)" },
+        { continent: "europe", medals: 1000, fill: "var(--color-europe)" },
+        { continent: "asia", medals: 500, fill: "var(--color-asia)" },
+        { continent: "australia", medals: 300, fill: "var(--color-australia)" },
+        { continent: "north_america", medals: 250, fill: "var(--color-north_america)" },
+        { continent: "south_america", medals: 150, fill: "var(--color-south_america)" },
+    ]);
+    const [sportsmanData, setSportsmanData] = useState<any[]>([]);
+    const [yearsData, setYearsData] = useState<any[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
         
-        {/* Main Content Area */}
-        <div className="flex h-screen flex-col w-4/5 p-5 overflow-y-auto">
-            {/* Header */}
-            <div className="flex justify-between items-center">
-                <h1 className="text-4xl font-bold text-accent mb-10">
-                    Analytics
-                </h1>
-            </div>
+            const API_URL = process.env.NEXT_PUBLIC_SPORTS_API_URL || "";
+            const token = Cookies.get("token");
+            const refreshToken = Cookies.get("refresh-token");
+        
+            try {
+                const medalsPerYearResponse = await fetch(
+                    `${API_URL}/medals/year`,
+                    {
+                        method: "GET",
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${token}`,
+                            "x-refresh-token": refreshToken || "",
+                        },
+                    }
+                );
+        
+                if (!medalsPerYearResponse.ok) {
+                    const errorData = await medalsPerYearResponse.json();
+                    router.push(`/error?code=${medalsPerYearResponse.status}&message=${errorData.message || "An error occurred while fetching data."}`);
+                }
+        
+                const data = await medalsPerYearResponse.json();
+
+                setYearsData(data.data.map((item: any) => ({
+                    year: item.Year,
+                    medals: item.total
+                })));
+
+                const medalsPerSportsmenResponse = await fetch(
+                    `${API_URL}/medals/sportsmen`,
+                    {
+                        method: "GET",
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${token}`,
+                            "x-refresh-token": refreshToken || "",
+                        },
+                    }
+                );
+        
+                if (!medalsPerSportsmenResponse.ok) {
+                    const errorData = await medalsPerSportsmenResponse.json();
+                    router.push(`/error?code=${medalsPerSportsmenResponse.status}&message=${errorData.message || "An error occurred while fetching data."}`);
+                }
+        
+                const data2 = await medalsPerSportsmenResponse.json();
+
+                setSportsmanData(data2.data.map((item: any) => ({
+                    sportsman: item.Name,
+                    gold: item.Gold,
+                    silver: item.Silver,
+                    bronze: item.Bronze,
+                    appearance: item.Total_Appearances
+                })));
+            } catch (err: any) {
+                console.error("Error fetching data:", err);
+                router.push(`/error?code=500&message=${err.message || "An unexpected error occurred."}`);
+            } finally {
+                setLoading(false);
+            }
+        };
+        
+        fetchData();
+    }, [])
+    
+    if (loading) {
+        return (
+          <LoadingSpinner />
+        )
+    }
+  
+    return (
+        <div className="h-full bg-gradient-to-tr from-primary-200 via-primary-200 to-primary-100 flex">
+            {/* SideBar */}
+            <SideBar />
             
-            <div className="flex flex-row justify-between items-center w-full">
-                <Card className="w-[50%] border-none bg-primary-50 text-white mr-5">
+            {/* Main Content Area */}
+            <div className="flex h-screen flex-col w-4/5 p-5 overflow-y-auto">
+                {/* Header */}
+                <div className="flex justify-between items-center">
+                    <h1 className="text-4xl font-bold text-accent mb-10">
+                        Analytics
+                    </h1>
+                </div>
+                
+                <div className="flex flex-row justify-between items-center w-full">
+                    <Card className="w-[50%] border-none bg-primary-50 text-white mr-5">
+                        <CardHeader>
+                        </CardHeader>
+
+                        <CardContent>
+                            <ChartContainer config={chartConfig} className="h-[400px] w-full">
+                                <BarChart 
+                                    accessibilityLayer 
+                                    data={sportsmanData}
+                                >
+                                    <XAxis
+                                        dataKey="sportsman"
+                                        type="category"
+                                        tickLine={false}
+                                        tickMargin={10}
+                                        axisLine={false}
+                                    />
+                                    
+                                    <ChartTooltip
+                                        cursor={false}
+                                        content={
+                                            <ChartTooltipContent
+                                                indicator="dot"
+                                                hideLabel
+                                                className="bg-primary-100 text-white p-2 border-none rounded-lg shadow-lg"
+                                            />
+                                        }
+                                    />
+                                    
+                                    <Bar dataKey="gold" fill="var(--color-gold)" radius={4} />
+                                    <Bar dataKey="silver" fill="var(--color-silver)" radius={4} />
+                                    <Bar dataKey="bronze" fill="var(--color-bronze)" radius={4} />
+                                    <Bar dataKey="appearance" fill="var(--color-appearance)" radius={4} />
+                                </BarChart>
+                            </ChartContainer>
+                        </CardContent>
+
+                        <CardFooter className="flex-col items-start gap-2 text-sm">
+                        </CardFooter>
+                    </Card>
+
+                    <Card className="flex flex-col w-[50%] border-none bg-primary-50 text-white ml-5">
+                        <CardHeader className="items-center pb-0">
+                        </CardHeader>
+
+                        <CardContent className="flex-1 pb-0">
+                            <ChartContainer
+                                config={continentChartConfig}
+                                className="aspect-square max-h-[450px] [&_.recharts-text]:fill-background"
+                            >
+                                <PieChart>
+                                    <ChartTooltip
+                                        content={
+                                            <ChartTooltipContent
+                                                nameKey="continent"
+                                                indicator="dot"
+                                                hideLabel
+                                                className="bg-primary-100 text-white p-2 border-none rounded-lg shadow-lg"
+                                            />
+                                        }
+                                    />
+
+                                    <Pie data={continentData} dataKey="medals">
+                                        <LabelList
+                                            dataKey="continent"
+                                            className="fill-background"
+                                            stroke="none"
+                                            fontSize={12}
+                                        />
+                                    </Pie>
+                                </PieChart>
+                            </ChartContainer>
+                        </CardContent>
+
+                        <CardFooter className="flex-col gap-2 text-sm">
+                        </CardFooter>
+                    </Card>
+                </div>
+
+                <Card className="w-full border-none bg-primary-50 text-white mt-10">
                     <CardHeader>
                     </CardHeader>
-
+                    
                     <CardContent>
-                        <ChartContainer config={chartConfig} className="h-[400px] w-full">
-                            <BarChart 
-                                accessibilityLayer 
-                                data={realChartData}
+                        <ChartContainer config={chartConfig} className="h-[500px] w-full">
+                            <AreaChart
+                                accessibilityLayer
+                                data={yearsData}
+                                margin={{
+                                    left: 12,
+                                    right: 12,
+                                }}
                             >
                                 <XAxis
-                                    dataKey="sportsman"
-                                    type="category"
+                                    dataKey="year"
                                     tickLine={false}
-                                    tickMargin={10}
                                     axisLine={false}
+                                    tickMargin={8}
                                 />
-                                
+
                                 <ChartTooltip
                                     cursor={false}
                                     content={
@@ -145,106 +287,24 @@ function Analytics() {
                                         />
                                     }
                                 />
-                                
-                                <Bar dataKey="gold" fill="var(--color-desktop)" radius={4} />
-                                <Bar dataKey="silver" fill="var(--color-mobile)" radius={4} />
-                                <Bar dataKey="bronze" fill="var(--color-desktop)" radius={4} />
-                                <Bar dataKey="appearance" fill="var(--color-mobile)" radius={4} />
-                            </BarChart>
-                        </ChartContainer>
-                    </CardContent>
 
-                    <CardFooter className="flex-col items-start gap-2 text-sm">
-                    </CardFooter>
-                </Card>
-
-                <Card className="flex flex-col w-[50%] border-none bg-primary-50 text-white ml-5">
-                    <CardHeader className="items-center pb-0">
-                    </CardHeader>
-
-                    <CardContent className="flex-1 pb-0">
-                        <ChartContainer
-                            config={continentChartConfig}
-                            className="aspect-square max-h-[450px] [&_.recharts-text]:fill-background"
-                        >
-                            <PieChart>
-                                <ChartTooltip
-                                    content={
-                                        <ChartTooltipContent
-                                            nameKey="continent"
-                                            indicator="dot"
-                                            hideLabel
-                                            className="bg-primary-100 text-white p-2 border-none rounded-lg shadow-lg"
-                                        />
-                                    }
+                                <Area
+                                    dataKey="medals"
+                                    type="linear"
+                                    fill="var(--color-medals)"
+                                    fillOpacity={0.4}
+                                    stroke="var(--color-medals)"
                                 />
-
-                                <Pie data={continentData} dataKey="medals">
-                                    <LabelList
-                                        dataKey="continent"
-                                        className="fill-background"
-                                        stroke="none"
-                                        fontSize={12}
-                                    />
-                                </Pie>
-                            </PieChart>
+                            </AreaChart>
                         </ChartContainer>
                     </CardContent>
-
-                    <CardFooter className="flex-col gap-2 text-sm">
+                    
+                    <CardFooter>
                     </CardFooter>
                 </Card>
             </div>
-
-            <Card className="w-full border-none bg-primary-50 text-white mt-10">
-                <CardHeader>
-                </CardHeader>
-                
-                <CardContent>
-                    <ChartContainer config={chartConfig} className="h-[500px] w-full">
-                        <AreaChart
-                            accessibilityLayer
-                            data={linearChartData}
-                            margin={{
-                                left: 12,
-                                right: 12,
-                            }}
-                        >
-                            <XAxis
-                                dataKey="year"
-                                tickLine={false}
-                                axisLine={false}
-                                tickMargin={8}
-                            />
-
-                            <ChartTooltip
-                                cursor={false}
-                                content={
-                                    <ChartTooltipContent
-                                        indicator="dot"
-                                        hideLabel
-                                        className="bg-primary-100 text-white p-2 border-none rounded-lg shadow-lg"
-                                    />
-                                }
-                            />
-
-                            <Area
-                                dataKey="medals"
-                                type="linear"
-                                // fill="var(--color-desktop)"
-                                fillOpacity={0.4}
-                                // stroke="var(--color-desktop)"
-                            />
-                        </AreaChart>
-                    </ChartContainer>
-                </CardContent>
-                
-                <CardFooter>
-                </CardFooter>
-            </Card>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Analytics;
